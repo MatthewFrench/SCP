7827bb6b41deb2f668eadb5c29fb47a32b6aa715
Matthew <matthewf@santoslab.org>
2015-11-30 15:36:57 -0600

    Got DDS working
    DDS works now. Has a few problems but it runs.

--- clinical-scenarios/insufflator-shutoff/scp/Run-as-DDS/Windows/runBPMonitor.ps1
+++ clinical-scenarios/insufflator-shutoff/scp/Run-as-DDS/Windows/runBPMonitor.ps1
@@ -7,9 +7,12 @@
 #  http://www.eclipse.org/legal/epl-v10.html
 #
 
-$env:NDDSHOME="C:\Program Files (x86)\RTI\ndds.5.1.0"
-$env:RTI_LICENSE_FILE="C:\Program Files (x86)\RTI\rti_license.dat"
-$env:PATH=$env:PATH + ";C:\Program Files (x86)\RTI\ndds.5.1.0\lib\x64Win64jdk"
+# $env:NDDSHOME="C:\Program Files (x86)\RTI\ndds.5.1.0"
+# $env:RTI_LICENSE_FILE="C:\Program Files (x86)\RTI\rti_license.dat"
+# $env:PATH=$env:PATH + ";C:\Program Files (x86)\RTI\ndds.5.1.0\lib\x64Win64jdk"
+
+$env:NDDSHOME="C:\Program Files\rti_connext_dds-5.2.0\lib\x64Win64VS2013"
+$env:RTI_LICENSE_FILE="C:\Program Files\rti_connext_dds-5.2.0\rti_license.dat"
 
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
@@ -21,8 +24,6 @@ cd ..
 cd ..
 
 cd "bpmonitor"
-
-$griffonScript = "./griffonw.bat"
 
 gvm v
 
